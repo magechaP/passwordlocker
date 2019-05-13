bd4a3d5e5ea6f58620efee4af65635b0ccf439c9
magechaP <magechapeter@gmail.com>
2019-05-13 17:10:19 +0300

    fix indentation issues

--- run.py
+++ run.py
@@ -3,50 +3,64 @@
 from user import User
 import random
 import pyperclip
-def create_user(p_name,u_name,ps):
-    '''
-    function to create a new user
-    '''
-    new_user = User(p_name,u_name,ps)
-    return new_user
+
+
+def create_user(p_name, u_name, ps):
+        '''
+        function to create a new user
+        '''
+        new_user = User(p_name, u_name, ps)
+        return new_user
+
+
 def save_user(user):
-    '''
-    function to save user
-    '''
-    user.save_user()
+        '''
+        function to save user
+        '''
+        user.save_user()
+
+
 def del_user(user):
-    '''
-    function to delete a user
-    '''
-    user.del_user()
+        '''
+        function to delete a user
+        '''
+        user.del_user()
+
+
 def find_user(username):
-    '''
-    function that finds a user by platform_name and returns the user
-    '''
-    return User.find_by_username(username)
+        '''
+        function that finds a user by platform_name and returns the user
+        '''
+        return User.find_by_username(username)
+
+
 def check_existing_user(username):
-    '''
-    function that check if a user exists with that platform_name and return a boolean
-    '''
-    return User.user_exists(username)
+        '''
+        function that check if a user exists with that platform_name and return a boolean
+        '''
+        return User.user_exists(username)
+
+
 def display_user():
-    '''
-    function that returns all the saved users
-    '''
-    return User.display_users()
+        '''
+        function that returns all the saved users
+        '''
+        return User.display_users()
+
+
 def copy_password(username):
-     '''
-     function to copy password to the clipboard
-    '''
-     my_password = UserData.show_user_data(username)
-     pyperclip.copy(my_password.password)
+         '''
+        function to copy password to the clipboard
+        '''
+         my_password = UserData.show_user_data(username)
+         pyperclip.copy(my_password.password)
 
 def main():
-    print('Hello! welcome to your password locker. what is your name?')
-    platform_name = input()
-    print(f'hello {platform_name} what do you want to acess?')
-    print('\n')
-    while True:
+        print('Hello! welcome to your password locker. what is your name?')
+        platform_name = input()
+        print(f'hello {platform_name} what do you want to acess?')
+        print('\n')
+        while True:
         print('Use these short codes: sp - save password, gp - generate password, cp - copy password, dp - display passwords, fp - find passwords, ex - exit ')
         short_code = input().lower()
         if short_code == 'sp':
@@ -126,5 +140,9 @@ def main():
             print('you did not use the short_code!!!')
 
 
- if __name__ == '__main__':
-     main()          
+
+
+    
+
+if __name__ == '__main__':
+    main()          
--- user.py
+++ user.py
@@ -3,36 +3,40 @@ class User:
     Class that generates new instances of contacts.
     """
 
-    user_list = [] # empty user list array
+    user_list = []  # empty user list array
 
-    def __init__(self,name,username,password):
+    def __init__(self, name, username, password):
         self.name = name
         self.username = username
-         self.password = password
+        self.password = password
 
         """
         __init__ method that helps us define properties for our objects.
-        """    
+        """
+
     def save_user(self):
         '''
         save_user method saves user objects into user_list
         '''
         User.user_list.append(self)
+
     def delete_user(self):
         '''
         delete_user_method deletes a saved user from the user_list
         '''
         User.user_list.remove(self)
+
     @classmethod
-    def find_by_username(cls,username):
+    def find_by_username(cls, username):
         '''
         method that takes in a username and returns a user that matches that username
         '''
         for user in cls.user_list:
             if user.username == username:
                 return user
+
     @classmethod
-    def user_exists(cls,username):
+    def user_exists(cls, username):
         '''
         method that checks if a user exists froom the user list
         '''
@@ -40,13 +44,13 @@ class User:
             if user.username == username:
                 return True
         return False
+
     @classmethod
     def display_users(cls):
         '''
         method that returns a list of all users saved
         '''
         return cls.user_list
-
 
 
 if __name__ == '__main__':
