5cef8bbaa33ec9bfcab77cc4728e4c571303dc79
magechaP <magechapeter@gmail.com>
2019-05-13 12:56:28 +0300

    add init method and save user method

--- user.py
+++ user.py
@@ -8,4 +8,13 @@ class User:
     def __init__(self,name,username,password):
         self.name = name
         self.username = username
-        self.password = password
+         self.password = password
+
+        """
+        __init__ method that helps us define properties for our objects.
+        """    
+    def save_user(self):
+        '''
+        save_user method saves user objects into user_list
+        '''
+        User.user_list.append(self)
