aab460ff40f30adb638cb1d725d52b938d22e8ae
magechaP <magechapeter@gmail.com>
2019-05-13 15:02:55 +0300

    add test class and its descripts,that test for user class behaviour

--- test.py
+++ test.py
@@ -2,3 +2,14 @@ import unittest
 from user import User
 from credential import credentials
 import pyperclip
+
+
+class TestUser(unittest.TestCase):
+    """
+    test class that defines test cases for the user class behaviours
+    """
+    def setUp(self):
+        """
+        set up method to run before each test case
+        """
+        self.new_user = User("facebook","manka","2999") # crreate user objects
